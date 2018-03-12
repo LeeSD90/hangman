a34c7678fd5551cc2b26920b765789e74fd158ec
LeeSD90 <lee.doyle90@gmail.com>
2018-03-12 13:25:03 +0100

    Created get_random_word method (and fixed how it workds whoops)

--- hangman.rb
+++ hangman.rb
@@ -1,7 +1,12 @@
-dictionary = File.readlines("5desk.txt")
-word = ""
-while(word.length < 5 || word.length > 12) do
-	random_line = rand(0..dictionary.size)
-	word = dictionary[random_line]
+def get_random_word()
+	dictionary = File.readlines("5desk.txt")
+	word = ""
+
+	until(word.length > 5 && word.length < 12)
+		random_line = rand(0..dictionary.size)
+		word = dictionary[random_line]
+	end
+	return word
 end
-puts word
+1000.times{puts get_random_word()}
+
