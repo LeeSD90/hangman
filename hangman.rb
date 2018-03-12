a33c9a74138c57b62096f70dcca4c6a670166214
LeeSD90 <lee.doyle90@gmail.com>
2018-03-12 13:18:46 +0100

    modified code to select only words between 5 and 12 characters in length

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,7 @@
 dictionary = File.readlines("5desk.txt")
-random_line = rand(0..dictionary.size)
-word = dictionary[random_line]
+word = ""
+while(word.length < 5 || word.length > 12) do
+	random_line = rand(0..dictionary.size)
+	word = dictionary[random_line]
+end
 puts word
