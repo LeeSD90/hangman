9f5c3c1e1cfbf066bb626913185b26d9da03916a
LeeSD90 <lee.doyle90@gmail.com>
2018-03-12 16:05:56 +0100

    Added more output for the display. Added todos for next steps

--- hangman.rb
+++ hangman.rb
@@ -120,11 +120,30 @@ puts %q{
 	end
 end
 
-word = get_random_word
+#todo error check userinput -> chars only & not already guessed
+#todo only increment stage on unsuccessful guess
+def draw_score(stage)
+	puts "\n"
+	@word.split("").each { |l|
+		if @chosen_letters.include?(l) then print l + " "
+		else print "_ "
+		end
+	} 
+	puts "\n\n"
+	puts "So far you have guessed - " + @chosen_letters.to_s.colorize(:green)
+	puts "You have "+(8-stage).to_s+" guesses remaining!\n"
+end
+
+@word = get_random_word.gsub("\n", '')
+@chosen_letters = []
 stage = 1
 
-until(stage == 9)
+until(stage == 8)
+	puts @word #todo remove
+	guess = gets.chomp
+	@chosen_letters.push(guess)
 	draw_stickman(stage)
+	draw_score(stage)
 	stage += 1
 end
 
